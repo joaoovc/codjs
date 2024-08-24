341efc1985a8c074d951826bb978bc8206084ec2
João Victor <140767588+joaoovc@users.noreply.github.com>
2024-08-23 21:58:27 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const rotas = require('./rotas');
+
+const app  = express();
+
+app.use(express.json());
+app.use('/api', rotas);
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+})
